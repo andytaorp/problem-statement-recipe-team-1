9d377f62db6c80314f0adbf7f44b1e83115161dc
MyGitHubAccount10 <23025668@myrp.edu.sg>
2025-02-04 11:13:52 +0800

    Added pages

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,42 @@
-const Home = () => {
+import { useEffect }from 'react'
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+import { useAuthContext } from "../hooks/useAuthContext"
 
-    return (
-      <div className="home">
-        <h2>Home</h2>
+// components
+import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
+
+const Home = () => {
+  const {workouts, dispatch} = useWorkoutsContext()
+  const {user} = useAuthContext()
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts', {
+        headers: {'Authorization': `Bearer ${user.token}`},
+      })
+      const json = await response.json()
+
+      if (response.ok) {
+        dispatch({type: 'SET_WORKOUTS', payload: json})
+      }
+    }
+
+    if (user) {
+      fetchWorkouts()
+    }
+  }, [dispatch, user])
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map((workout) => (
+          <WorkoutDetails key={workout._id} workout={workout} />
+        ))}
       </div>
-    )
-  }
-  
-  export default Home
+      <WorkoutForm />
+    </div>
+  )
+}
+
+export default Home
